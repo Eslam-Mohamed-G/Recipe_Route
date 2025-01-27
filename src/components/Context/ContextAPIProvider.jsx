22b7f335593edd1502a79972dcc53090668c1452
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-27 17:11:03 +0200

    add axios in Countext api to handle All API

--- src/components/Context/ContextAPIProvider.jsx
+++ src/components/Context/ContextAPIProvider.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { createContext, useState, useEffect  } from 'react';
+import axios from "axios";
 
-function ContextAPIProvider() {
+export const dataContext = createContext();
+
+function ContextAPIProvider({ children }) {
+    const [meal, setMeal] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    // List all meal categories
+    const fetchMeal = async ()=>{
+        setLoading(true);
+        try {
+            const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
+            setMeal(response?.data)
+            console.log(response?.data?.categories);
+        } catch (error) {
+            console.error('Error fetching categories:', error);
+        } finally {
+            setLoading(false);
+        };
+    };
+
+    useEffect(() => {
+        fetchMeal();
+        return () => {
+            
+        };
+    }, []);
     return (
-        <div>
-
-        </div>
+        <dataContext.Provider value={{ meal, setMeal, loading }}>
+            {children}
+        </dataContext.Provider>
     )
 }
 
