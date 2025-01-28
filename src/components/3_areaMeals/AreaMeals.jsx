40b5eb720d73a63f37735145dba21361e5ebb916
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-29 00:04:20 +0200

    make AreaMeals responsive

--- src/components/3_areaMeals/AreaMeals.jsx
+++ src/components/3_areaMeals/AreaMeals.jsx
@@ -4,17 +4,21 @@ import { dataContext } from '../Context/ContextAPIProvider';
 function AreaMeals() {
     const { mealsArea, loading } = useContext(dataContext);
     return (
-        <div className='container'>
-            {mealsArea?.map((meals) => (
-                <div className='card' key={meals.idMeal}>
-                    <figure className='card-img-top' >
-                        <img src={meals.strMealThumb} alt={meals.strMeal} />
-                    </figure>
-                    <div className='card-body'>
-                        <p className='text-center'>{meals.strMeal}</p>
-                    </div>
-                </div>
-            ))}
+        <div className='areaMeals container'>
+            <div className='row'>
+                {mealsArea?.map((meals) => (
+                    // <div className='container'></div>
+                        <div className='card' key={meals.idMeal}>
+                            <figure className='card-img-top' >
+                                <img src={meals.strMealThumb} alt={meals.strMeal} />
+                            </figure>
+                            <div className='card-body'>
+                                <p className='text-center'>{meals.strMeal.split(' ').slice(0, 4).join(' ')}</p>
+                            </div>
+                        </div>
+                    
+                ))}
+            </div>
         </div>
     )
 }
