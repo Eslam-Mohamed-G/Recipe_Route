99b6c023a7c951a6f9c27c1e6eb9f443e0b2627d
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-28 18:47:49 +0200

    add BrowserCountry to shom meals by Country

--- src/components/2_home/BrowserCountry.jsx
+++ src/components/2_home/BrowserCountry.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import ar from '../../assets/counters/ar.png';
+import it from '../../assets/counters/it.png';
 import cn from '../../assets/counters/cn.png';
 import eg from '../../assets/counters/eg.png';
 import fr from '../../assets/counters/fr.png';
@@ -12,12 +12,10 @@ import mx from '../../assets/counters/mx.png';
 import nl from '../../assets/counters/nl.png';
 import pl from '../../assets/counters/pl.png';
 import ru from '../../assets/counters/ru.png';
-import sk from '../../assets/counters/sk.png';
 import th from '../../assets/counters/th.png';
 import tr from '../../assets/counters/tr.png';
 import us from '../../assets/counters/us.png';
 import ca from '../../assets/counters/ca.png';
-import dz from '../../assets/counters/dz.png';
 import es from '../../assets/counters/es.png';
 import gb from '../../assets/counters/gb.png';
 import hr from '../../assets/counters/hr.png';
@@ -27,53 +25,47 @@ import ma from '../../assets/counters/ma.png';
 import my from '../../assets/counters/my.png';
 import ph from '../../assets/counters/ph.png';
 import pt from '../../assets/counters/pt.png';
-import sa from '../../assets/counters/sa.png';
-import sy from '../../assets/counters/sy.png';
 import tn from '../../assets/counters/tn.png';
 import ua from '../../assets/counters/UA.png';
 import vn from '../../assets/counters/vn.png';
 
 function BrowserCountry() {
   const country = [
-    { id: 1, imgSrc: ar, name: 'ar' },
-    { id: 2, imgSrc: cn, name: 'cn' },
-    { id: 3, imgSrc: eg, name: 'eg' },
-    { id: 4, imgSrc: fr, name: 'fr' },
-    { id: 5, imgSrc: gr, name: 'gr' },
-    { id: 6, imgSrc: ie, name: 'ie' },
-    { id: 7, imgSrc: jm, name: 'jm' },
-    { id: 8, imgSrc: kn, name: 'kn' },
-    { id: 9, imgSrc: mx, name: 'mx' },
-    { id: 10, imgSrc: nl, name: 'nl' },
-    { id: 11, imgSrc: pl, name: 'pl' },
-    { id: 12, imgSrc: ru, name: 'ru' },
-    { id: 13, imgSrc: sk, name: 'sk' },
-    { id: 14, imgSrc: th, name: 'th' },
-    { id: 15, imgSrc: tr, name: 'tr' },
-    { id: 16, imgSrc: us, name: 'us' },
-    { id: 17, imgSrc: ca, name: 'ca' },
-    { id: 18, imgSrc: dz, name: 'dz' },
-    { id: 19, imgSrc: es, name: 'gb' },
-    { id: 20, imgSrc: hr, name: 'hr' },
-    { id: 21, imgSrc: In, name: 'In' },
-    { id: 22, imgSrc: jp, name: 'jp' },
-    { id: 23, imgSrc: ma, name: 'ma' },
-    { id: 24, imgSrc: my, name: 'my' },
-    { id: 25, imgSrc: ph, name: 'ph' },
-    { id: 26, imgSrc: pt, name: 'pt' },
-    { id: 27, imgSrc: sa, name: 'sa' },
-    { id: 28, imgSrc: sy, name: 'sy' },
-    { id: 29, imgSrc: tn, name: 'tn' },
-    { id: 30, imgSrc: ua, name: 'ua' },
-    { id: 31, imgSrc: vn, name: 'vn' },
-    { id: 32, imgSrc: gb, name: 'gb' },
+    { id: 1, imgSrc: gb, name: 'British' },
+    { id: 2, imgSrc: cn, name: 'Chinese' },
+    { id: 3, imgSrc: eg, name: 'Egyptian' },
+    { id: 4, imgSrc: fr, name: 'French' },
+    { id: 5, imgSrc: gr, name: 'Greek' },
+    { id: 6, imgSrc: ie, name: 'Irish' },
+    { id: 7, imgSrc: jm, name: 'Jamaican' },
+    { id: 8, imgSrc: kn, name: 'Kenyan' },
+    { id: 9, imgSrc: mx, name: 'Mexican' },
+    { id: 10, imgSrc: nl, name: 'Dutch' },
+    { id: 11, imgSrc: pl, name: 'Polish' },
+    { id: 12, imgSrc: ru, name: 'Russian' },
+    { id: 13, imgSrc: th, name: 'Thai' },
+    { id: 14, imgSrc: tr, name: 'Turkish' },
+    { id: 15, imgSrc: us, name: 'American' },
+    { id: 16, imgSrc: ca, name: 'Canadian' },
+    { id: 17, imgSrc: es, name: 'Spanish' },
+    { id: 18, imgSrc: hr, name: 'Croatian' },
+    { id: 19, imgSrc: In, name: 'Indian' },
+    { id: 20, imgSrc: jp, name: 'Japanese' },
+    { id: 21, imgSrc: ma, name: 'Moroccan' },
+    { id: 22, imgSrc: my, name: 'Malaysian' },
+    { id: 23, imgSrc: ph, name: 'Filipino' },
+    { id: 24, imgSrc: pt, name: 'Portuguese' },
+    { id: 25, imgSrc: tn, name: 'Tunisian' },
+    { id: 26, imgSrc: ua, name: 'Ukrainian' },
+    { id: 27, imgSrc: vn, name: 'Vietnamese' },
+    { id: 28, imgSrc: it, name: 'Italian' },
   ]
   return (
     <div className='container'>
       <h1>Browse Country </h1>
       <div className='country-div'>
         {country?.map((element) => (
-          <NavLink key={element.id} className='country-link'>
+          <NavLink to={element.name} key={element.id} className='country-link'>
             <img src={element.imgSrc} alt={element.name} />
           </NavLink>
         ))}
--- src/components/Context/ContextAPIProvider.jsx
+++ src/components/Context/ContextAPIProvider.jsx
@@ -7,7 +7,7 @@ function ContextAPIProvider({ children }) {
     const [meal, setMeal] = useState([]);
     const [loading, setLoading] = useState(false);
 
-    // List all meal categories
+    // List all meal categories for ResponsiveSlider in Home Component
     const fetchMeal = async ()=>{
         setLoading(true);
         try {
@@ -20,9 +20,23 @@ function ContextAPIProvider({ children }) {
             setLoading(false);
         };
     };
+    
+    const fetchArea = async ()=>{
+        setLoading(true);
+        try {
+            const response = await axios.get('https://www.themealdb.com/api/json/v1/1/list.php?a=list');
+            setMeal(response?.data?.meals)
+            console.log(response?.data?.meals);
+        } catch (error) {
+            console.error('Error fetching categories:', error);
+        } finally {
+            setLoading(false);
+        };
+    };
 
     useEffect(() => {
         fetchMeal();
+        fetchArea();
         return () => {
             
         };
