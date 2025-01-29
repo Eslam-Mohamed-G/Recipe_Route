341746b7eb5e87e1eb03a093954fd8b5dd51e4d4
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-29 07:23:48 +0200

    handle Navbar and his style

--- src/components/1_navbar/Navbar.jsx
+++ src/components/1_navbar/Navbar.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Logo from '../../assets/logo.png'
+import MenuOpen from '../../assets/open.png'
+import MenuClose from '../../assets/close.png'
 import { NavLink } from 'react-router';
 
 function Navbar() {
+    const [menuToggle, setMenuToggle] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
+    const handleMenu = () => { setMenuToggle(!menuToggle); setIsOpen(!isOpen) }
     return (
         <div className='navbar'>
             <div className='container'>
-            <img src={Logo} alt="logo" className='logo' />
-            <button className="navbar-toggler" type="button">
-                <span className="navbar-toggler-icon" />
-            </button>
-            <div className="navbar-collapse">
-                <ul className="navbar-nav">
-                    <NavLink className="nav-item">
-                        <a className="nav-link active" aria-current="page" href="#">Meals</a>
-                    </NavLink>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Ingredients</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link">Area</a>
-                    </li>
-                </ul>
-            </div>
+                <div className='row'>
+                    <div className='navbar-menu'>
+                        <NavLink to={'/'}>
+                            <img src={Logo} alt="logo" className='logo' />
+                        </NavLink>
+                        <button className="navbar-toggler" type="button" onClick={handleMenu}>
+                            {menuToggle ? <img src={MenuOpen} alt="MenuOpen" className='toggler-img' /> : <img src={MenuClose} alt="MenuClose" className='toggler-img' />}
+                        </button>
+                    </div>
+                    <div className={`navbar-collapse ${!isOpen ? 'menuOpen' : 'menuClose'}`}>
+                        <ul className="navbar-nav isOpen">
+                            <li className="nav-item">
+                                <NavLink className="nav-link active">Meals</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link">Ingredients</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className="nav-link">Area</NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
         </div>
     )
