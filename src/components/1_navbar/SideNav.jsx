10d6d9bf4b39ed5ac22291b02459e6ffae6b83af
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-01-31 14:23:01 +0200

    handle error in meals component

--- src/components/1_navbar/SideNav.jsx
+++ src/components/1_navbar/SideNav.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from 'react';
 import Logo from '../../assets/logo.png';
 import Favicon from '../../assets/favicon.png';
-import { NavLink, useNavigate } from 'react-router-dom';
+import { NavLink, useNavigate, useParams } from 'react-router-dom';
 import { dataContext } from '../Context/ContextAPIProvider';
 
 function SideNav() {
     const navigate = useNavigate();
+    const { category } = useParams();
     return (
         <div>
             <NavLink to={'/'}>
@@ -14,7 +15,7 @@ function SideNav() {
 
             <ul className="navbar-nav">
                 <li className="nav-item">
-                    <NavLink to='/meals/beef' className="nav-link"><img src={Favicon} alt="nav-logo" className='nav-logo' />Meals</NavLink>
+                    <NavLink to={`/meals/${category}`} className="nav-link"><img src={Favicon} alt="nav-logo" className='nav-logo' />Meals</NavLink>
                 </li>
                 <li className="nav-item">
                     <NavLink to='/ingredients' className="nav-link"><img src={Favicon} alt="nav-logo" className='nav-logo' />Ingredients</NavLink>
