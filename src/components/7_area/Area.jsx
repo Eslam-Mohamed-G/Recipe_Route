e529a622e7cfefdaba3025193dbbba1bb3757414
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2025-02-01 08:09:09 +0200

    add style to area component

--- src/components/7_area/Area.jsx
+++ src/components/7_area/Area.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import BrowserCountry from '../2_home/BrowserCountry';
+import Footer from '../8_footer/Footer';
+import Navbar from '../1_navbar/Navbar';
 
 function Area() {
   return (
-    <div>
+    <div className='area-container'>
+      <Navbar/>
+      <div className='content'>
+
       <BrowserCountry/>
+      </div>
+      <div className='footer'>
+      <Footer/>
+
+      </div>
     </div>
   )
 }
